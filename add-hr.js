2c97ce2b709d5a7a70b0de6469b5d4241990b35c
Soxrox12 <hannahsox12@gmail.com>
2019-08-05 11:03:48 -0500

    Update add-hr.js
    updating js file for adding heart rate

--- add-hr.js
+++ add-hr.js
@@ -1,3 +1,5 @@
+
+
 var dps = [];  //dataPoints.
   var chart = new CanvasJS.Chart("chartContainer",{
     title :{
@@ -9,14 +11,19 @@ var dps = [];  //dataPoints.
    }]
 });
 
-function addDataPointsAndRender() {
-    xValue = Number(document.getElementById('xValue').value);
-    yValue = Number(document.getElementById('yValue').value);
-    dps.push({
-       x: xValue,
-        y: yValue
-   });
-    chart.render();
-}
- var renderButton = document.getElementById('renderButton');
-renderButton.addEventListener('click', addDPsAndRender);
+// function addDataPointsAndRender() {
+//     console.log("in add data");
+//     console.log(document.getElementById('xValue'));
+//     xValue = Number(document.getElementById('xValue').value);
+//     yValue = Number(document.getElementById('yValue').value);
+//     dps.push({
+//        x: xValue,
+//        y: yValue
+//    });
+//     // chart.render();
+//     return 0;
+// }
+
+var renderButton = document.getElementById('renderButton');
+console.log(renderButton);
+//renderButton.addEventListener('click', addDataPointsAndRender());
