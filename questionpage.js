26829abd1eb0f0293e0be3735805848d6750b6df
krazykangaroo <53009462+krazykangaroo@users.noreply.github.com>
2019-08-07 15:55:33 -0500

    Update questionpage.js

--- questionpage.js
+++ questionpage.js
@@ -3,15 +3,6 @@ var word = [];
 var journal = [];
 var color = [];
 
-// var datesInput=document.getElementById('question1');
-// console.log(datesInput);
-// var wordInput=document.getElementById('question2');
-// console.log(wordInput);
-// var journalInput=document.getElementById('question3');
-// console.log(journalInput);
-//
-// var messagebox=document.getElementById('added-answers');
-
 var datesInput;
 var wordInput;
 var journalInput;
@@ -27,36 +18,6 @@ function insert() {
   datesInput=document.getElementById('question1');
   wordInput=document.getElementById('question2');
   journalInput=document.getElementById('question3');
-  //var bird = document.getElementById('hahaha').value;
-  //var avgOn = document.getElementById('average').name;
-  //console.log(bird);
-  // console.log("HAPPY VAL: ", document.getElementById('happy').value);
-  // console.log("AVG VAL: ", document.getElementById('average').value);
-  // if(document.getElementById('happy').value == "on") {
-  //   usercolor = "happy";
-  // }
-  // else if (document.getElementById('average').value == "on") {
-  //   usercolor = "average";
-  // }
-  // else if (document.getElementById('annoyed').value=="on") {
-  //   usercolor = "annoyed";
-  // }
-  // else if (document.getElementById('anger').value=="on") {
-  //   usercolor = "anger";
-  // }
-  // else if (document.getElementById('sad').value=="on") {
-  //   usercolor = "sad";
-  // }
-  // else if (document.getElementById('anxious').value=="on") {
-  //   usercolor = "anxious";
-  // }
-  // else if (document.getElementById('tired').value=="on") {
-  //   usercolor = "tired";
-  // }
-  // else {
-  //   console.log("in else");
-  //   usercolor = "";
-  // }
 
   console.log("This is the color:", usercolor);
 
